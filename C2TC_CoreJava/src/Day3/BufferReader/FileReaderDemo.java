cbfec5eee4312375f071dfafe60838bcb9b7f1f9
mershanpeter <mershanpeter27@gmail.com>
2025-08-18 14:14:16 +0530

    Day4

---
+++ C2TC_CoreJava/src/Day3/BufferReader/FileReaderDemo.java
@@ -0,0 +1,17 @@
+package Day3.BufferReader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderDemo {
+	public static void main(String args[]) throws IOException{
+		FileReader fr = new FileReader("data.txt");
+		BufferedReader br = new BufferedReader(fr);
+		String content;
+		while((content = br.readLine()) != null){
+		System.out.println(content);
+		
+	}
+	}
+}
