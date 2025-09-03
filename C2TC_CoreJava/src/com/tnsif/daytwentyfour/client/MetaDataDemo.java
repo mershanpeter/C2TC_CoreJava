bc6887ee0d3df5dbe6b98398dcafeac74985d438
mershanpeter <mershanpeter27@gmail.com>
2025-09-03 19:14:44 +0530

    Day 24

---
+++ C2TC_CoreJava/src/com/tnsif/daytwentyfour/client/MetaDataDemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.daytwentyfour.client;
+
+import com.tnsif.daytwentyfour.metadata.DBMetadata;
+
+public class MetaDataDemo {
+
+	public static void main(String[] args) {
+		DBMetadata.showDatabaseMetataData();
+		DBMetadata.showRSMetadata();
+
+	}
+
+}
---
+++ C2TC_CoreJava/src/com/tnsif/daytwentyfour/transaction/BankTestCases.java
@@ -0,0 +1,21 @@
+package com.tnsif.daytwentyfour.transaction;
+
+import static com.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class BankTestCases {
+	BankTransaction trans1;
+
+	@ParameterizedTest
+	@ValueSource(ints= {1001, 1006})
+	void validateAccountTest(int accNo) {
+		trans1 = new BankTransaction();
+		boolean status = trans1.validateAccount(accNo);
+	//	assertEquals(true, status);
+		assertTrue(status);
+	}
+
+}
