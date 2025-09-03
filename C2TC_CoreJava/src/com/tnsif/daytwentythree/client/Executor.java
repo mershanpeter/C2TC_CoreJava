d11ca5452f242735a30d54327edd4b21ab530301
mershanpeter <mershanpeter27@gmail.com>
2025-09-03 19:07:19 +0530

    Day 23

---
+++ C2TC_CoreJava/src/com/tnsif/daytwentythree/client/Executor.java
@@ -0,0 +1,19 @@
+package com.tnsif.daytwentythree.client;
+
+import com.tnsif.daytwentytwo.preparedstatementinterface.PreparedStatementDemo;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		PreparedStatementDemo.showEmp();
+		PreparedStatementDemo.updateEmpName(3, "Neha");
+		PreparedStatementDemo.updateEmpSalary(3, 65000);
+		PreparedStatementDemo.showEmp();
+		if(!PreparedStatementDemo.deleteEmp(8));
+			System.err.println("No such employee...");
+	
+		PreparedStatementDemo.showEmp();
+
+	}
+
+}
